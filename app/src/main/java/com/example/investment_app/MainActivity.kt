d2948f8d2fb7b109aa872589c21a9f1e4a1e7ed8
Ender <sim200347@gmail.com>
2022-03-26 12:50:36 +0300

    Add base application structure

---
+++ app/src/main/java/com/example/investment_app/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.investment_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.investment_app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        bindingClass.buttonProfile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+        bindingClass.buttonNews.setOnClickListener {
+            val intent = Intent(this, NewsActivity::class.java)
+            startActivity(intent)
+        }
+        bindingClass.buttonPortfolio.setOnClickListener {
+            val intent = Intent(this, PortfolioActivity::class.java)
+            startActivity(intent)
+        }
+        bindingClass.buttonSearch.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
