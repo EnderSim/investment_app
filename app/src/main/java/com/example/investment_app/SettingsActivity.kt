2e0ea361a8858b8ef7b7752d1772d5fe50b343d0
Ruslan <ruskura@icloud.com>
2022-05-25 20:01:57 +0300

    сделал вкладку настроек и уведомлений

--- app/src/main/java/com/example/investment_app/SettingsActivity.kt
+++ app/src/main/java/com/example/investment_app/SettingsActivity.kt
@@ -1,11 +1,39 @@
 package com.example.investment_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.investment_app.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivitySettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bindingClass = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_settings)
+        bindingClass.buttonnotif.setOnClickListener {
+            object {
+                val NOTIFICATION_ID = 101
+                val CHANNEL_ID = "channelID"
+            }
+
+            fun onCreate(savedInstanceState: Bundle?) {
+                onCreate(savedInstanceState)
+                setContentView(R.layout.activity_main)
+
+                val builder = NotificationCompat.Builder(this, "channelID")
+                    .setSmallIcon(android.R.drawable.alert_dark_frame)
+                    .setContentTitle("Title")
+                    .setContentText("Text")
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+                with(NotificationManagerCompat.from(this)) {
+                    notify(101, builder.build()) // посылаем уведомление
+                }
+            }
+        }
     }
 }
